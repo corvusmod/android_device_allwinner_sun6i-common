c79e3780b89d2d2b2d9bbec167562d804017cb34
Fredrik Karlsson <fkrlsn@gmail.com>
2013-02-07 04:12:24 +0100

    Initial commit (not usable)

---
+++ BoardCommonConfig.mk
@@ -0,0 +1,104 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET_BOOTLOADER_BOARD_NAME := a31
+TARGET_BOARD_PLATFORM := sun6i
+
+USE_CAMERA_STUB := false
+BOARD_HAS_SDCARD_INTERNAL := true
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# CPU
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a7
+TARGET_ARCH_VARIANT_FPU := neon
+ARCH_ARM_HAVE_VFP := true
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_ARMV7A := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# CFLAGS
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp
+
+TARGET_PROVIDES_INIT := true
+TARGET_PROVIDES_INIT_RC := false
+TARGET_PROVIDES_INIT_TARGET_RC := true
+TARGET_RECOVERY_INITRC := device/allwinner/sun6i-common/recovery/init.recovery.rc
+
+# Kernel
+# TARGET_KERNEL_SOURCE := kernel/sunxi
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_KERNEL_CMDLINE := console=ttyS0,115200 rw init=/init loglevel=4 ion_reserve=256M
+
+# Custom kernel toolchain
+# TARGET_KERNEL_CUSTOM_TOOLCHAIN :=
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_SYSTEMIMAGES_USE_EXT3 := true
+#TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+
+# Graphics
+USE_OPENGL_RENDERER := true
+TARGET_DISABLE_TRIPLE_BUFFERING := true
+BOARD_EGL_CFG := device/allwinner/sun6i-common/configs/egl.cfg
+ENABLE_WEBGL := true
+BOARD_USE_SKIA_LCDTEXT := true
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
+# HWComposer
+BOARD_USES_HWCOMPOSER := true
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+BOARD_WITH_ALSA_UTILS := true
+TARGET_PROVIDES_LIBAUDIO := true
+BOARD_USES_GENERIC_AUDIO := false
+
+# Wifi
+BOARD_WIFI_VENDOR                := realtek
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_rtl
+BOARD_HOSTAPD_DRIVER             := WEXT
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_rtl
+BOARD_WLAN_DEVICE                := rtl8192cu
+
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/8192cu.ko"
+WIFI_DRIVER_MODULE_NAME          := 8192cu
+
+#TARGET_CUSTOM_WIFI := ../../hardware/realtek/wlan/wifi_realtek.c
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+TARGET_CUSTOM_BLUEDROID := ../../../device/allwinner/sun6i-common/bluetooth/bluedroid.c
+
+# Recovery
+TARGET_NO_RECOVERY := false
+BOARD_HAS_NO_SELECT_BUTTON := false
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_MISC_PARTITION := false
+
+# inherit from the proprietary version
+-include vendor/allwinner/sun6i-common/BoardConfigVendor.mk
---
+++ common.mk
@@ -0,0 +1,133 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+COMMON_PATH := device/sun6i-common
+
+DEVICE_PACKAGE_OVERLAYS := $(COMMON_PATH)/overlay
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+PRODUCT_COPY_FILES += \
+    device/allwinner/sun6i-common/init.sun6i.usb.rc:root/init.sun6i.usb.rc \
+    device/allwinner/sun6i-common/ueventd.sun6i.rc:root/ueventd.sun6i.rc
+
+# Firmware
+PRODUCT_COPY_FILES += \
+    device/allwinner/sun6i-common/prebuilt/firmware/ct36x-sc5012-v10.bin:system/vendor/firmware/ct36x-sc5012-v10.bin \
+    device/allwinner/sun6i-common/prebuilt/firmware/gsl3680-sc5075-hs-jg-gg.bin:system/vendor/firmware/gsl3680-sc5075-hs-jg-gg.bin \
+    device/allwinner/sun6i-common/prebuilt/firmware/gslX680-sc5009-v99.bin:system/vendor/firmware/gslX680-sc5009-v99.bin
+
+# Hardware-specific features
+PRODUCT_COPY_FILES += \
+    device/allwinner/sun6i-common/configs/permissions/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    device/allwinner/sun6i-common/configs/permissions/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    device/allwinner/sun6i-common/configs/permissions/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    device/allwinner/sun6i-common/configs/permissions/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    device/allwinner/sun6i-common/configs/permissions/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+    device/allwinner/sun6i-common/configs/permissions/android.hardware.faketouch.xml:system/etc/permissions/android.hardware.faketouch.xml \
+    device/allwinner/sun6i-common/configs/permissions/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    device/allwinner/sun6i-common/configs/permissions/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    device/allwinner/sun6i-common/configs/permissions/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    device/allwinner/sun6i-common/configs/permissions/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    device/allwinner/sun6i-common/configs/permissions/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    device/allwinner/sun6i-common/configs/permissions/android.hardware.live_wallpaper.xml:system/etc/permissions/android.hardware.live_wallpaper.xml \
+    device/allwinner/sun6i-common/configs/permissions/android.software.pppoe.xml:system/etc/permissions/android.software.pppoe.xml \
+    device/allwinner/sun6i-common/configs/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+    device/allwinner/sun6i-common/configs/permissions/platform.xml:system/etc/permissions/android.hardware.platform.xml \
+    device/allwinner/sun6i-common/configs/permissions/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+
+# WiFi
+PRODUCT_COPY_FILES += \
+    device/allwinner/sun6i-common/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+# Vold/Storage
+PRODUCT_COPY_FILES += \
+    device/allwinner/sun6i-common/configs/vold.fstab:system/etc/vold.fstab
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    device/allwinner/sun6i-common/recovery/root/keyboard.ko:recovery/keyboard.ko \
+    device/allwinner/sun6i-common/recovery/root/nand.ko:recovery/nand.ko \
+    device/allwinner/sun6i-common/recovery/root/touch.ko:recovery/touch.ko
+
+# Audio/Alsa
+#PRODUCT_COPY_FILES += \
+
+# Build extra packages
+
+# Audio
+#PRODUCT_PACKAGES += \
+#    audio.a2dp.default \
+#    audio.usb.default \
+#    tinyplay
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    hciconfig \
+    hcitool \
+    bttest \
+    l2ping
+
+# Filesystem management
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs
+
+# USB
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+
+# Graphics
+PRODUCT_PACKAGES += \
+    librs_jni
+
+# Extra packages
+PRODUCT_PACKAGES += \
+    busybox \
+    Camera \
+    dbus-send
+
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.service.adb.enable=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y \
+    ro.opengles.version=131072 \
+    hwui.render_dirty_regions=false \
+    wifi.supplicant_scan_interval=15
+
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    persist.sys.timezone=Europe/Stockholm \
+    ro.allow.mock.location=1 \
+    ro.kernel.android.checkjni=1 \
+    sys.hwc.compose_policy=6 \
+    testing.mediascanner.skiplist=/mnt/sdcard/Android/
+
+PRODUCT_CHARACTERISTICS := tablet
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# Inherit tablet dalvik settings
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+
+# Include google apps
+$(call inherit-product-if-exists, vendor/google/gapps.mk)
+
+# Setup proprietary files
+$(call inherit-product-if-exists, vendor/allwinner/sun6i-common/common-vendor.mk)
